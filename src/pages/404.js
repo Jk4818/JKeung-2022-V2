2733c4ef0b272cbd17ee25d97863c14d5e38c65b
Jason Keung <jasonkeung@Jasons-MacBook-Pro.local>
2021-12-22 17:53:27 +0000

    added hero page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,14 +3,17 @@ import SquareButton from "../components/SquareButton";
 
 const NotFoundPage = () => {
   return (
-    <main className="w-screen h-screen bg-stone-900 flex flex-col justify-center items-center font-roboto font-bold">
+    <main className="w-screen h-screen bg-dark-gray flex flex-col justify-center items-center gap-6 font-roboto font-bold">
       <title>Not found</title>
-      <h1 className="text-gray-600 text-massive">404</h1>
+
+      <h1 className="text-blue-gray text-9xl sm:text-massive">404</h1>
       <h2 className="text-white text-4xl">Page Not Found</h2>
-      <p className="text-stone-400 font-roboto-mono font-medium">
+      <p className="text-p-gray font-roboto-mono font-medium">
         You're accessing an unknown page :c
       </p>
-      <SquareButton>Back to home</SquareButton>
+      <div className="mt-6">
+        <SquareButton color="white">Back to home</SquareButton>
+      </div>
     </main>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        'dark-gray': '#141413',
+        'p-gray' : '#888888',
+        'blue-gray' : '#6C757D',
+        'vibrant-orange' : '#E78A00',
+      },
       fontFamily: {
         'roboto': ["roboto", "sans-serif"],
         'roboto-mono': ["roboto-mono", "monospace"]
