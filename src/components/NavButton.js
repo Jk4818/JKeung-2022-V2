ca8f8378984a6927cf50389eb49df14576300b08
Jason Keung <jk4818@ic.ac.uk>
2021-12-27 18:37:06 +0000

    bug: corrected navbutton

--- src/components/NavButton.js
+++ src/components/NavButton.js
@@ -4,36 +4,36 @@ function NavButton({state}) {
 
   return (
     <div className="w-8 h-8">
-      <div className={`w-full h-full flex flex-wrap overflow-hidden ${state && `-rotate-180`} transition ease-in-out duration-300`}>
-        <div className="w-1/2 p-0.5 w-1/3 overflow-hidden">
+      <div className={`w-full h-full grid grid-cols-3 flex-wrap gap-1 overflow-hidden ${state && `-rotate-180`} transition ease-in-out duration-300`}>
+        <div className="w-full aspect-square overflow-hidden">
           <div className="w-full h-full bg-white"></div>
         </div>
-        <div className="w-1/2 p-0.5 w-1/3 overflow-hidden">
+        <div className="w-full aspect-square overflow-hidden">
           <div className={`w-full h-full ${state ? `bg-tansparent` : `bg-white`} transition ease-in-out duration-300`}></div>
         </div>
-        <div className="w-1/2 p-0.5 w-1/3 overflow-hidden">
+        <div className="w-full aspect-square overflow-hidden">
           <div className="w-full h-full bg-white"></div>
         </div>
 
-        <div className="w-1/2 p-0.5 w-1/3 overflow-hidden">
+        <div className="w-full aspect-square overflow-hidden">
           <div className={`w-full h-full ${state ? `bg-tansparent` : `bg-white`} transition ease-in-out duration-300`}></div>
         </div>
 
-        <div className="w-1/2 p-0.5 w-1/3 overflow-hidden">
+        <div className="w-full aspect-square overflow-hidden">
           <div className="w-full h-full bg-white"></div>
         </div>
-        <div className="w-1/2 p-0.5 w-1/3 overflow-hidden">
+        <div className="w-full aspect-square overflow-hidden">
           <div className={`w-full h-full ${state ? `bg-tansparent` : `bg-white`} transition ease-in-out duration-300`}></div>
         </div>
 
-        <div className="w-1/2 p-0.5 w-1/3 overflow-hidden">
+        <div className="w-full aspect-square overflow-hidden">
           <div className="w-full h-full bg-white"></div>
         </div>
-        <div className="w-1/2 p-0.5 w-1/3 overflow-hidden">
+        <div className="w-full aspect-square overflow-hidden">
           <div className={`w-full h-full ${state ? `bg-tansparent` : `bg-white`} transition ease-in-out duration-300`}></div>
         </div>
 
-        <div className="w-1/2 p-0.5 w-1/3 overflow-hidden">
+        <div className="w-full aspect-square overflow-hidden">
           <div className="w-full h-full bg-white"></div>
         </div>
       </div>
